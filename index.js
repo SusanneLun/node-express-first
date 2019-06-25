c0d60c5639fe27bc61c144bc9a0322421c18ad47
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-06-25 12:49:16 +0100

    requesting using find

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express')
 const app = express()
 
+const courses = [
+  { id: 1, name: 'course1' },
+  { id: 2, name: 'course2' },
+  { id: 3, name: 'course3' }
+]
+
 app.get('/', (req, res) => {
   res.send('Hello Entire World')
 })
 
 app.get('/api/courses', (req, res) => {
-  res.send([1, 2, 3])
+  res.send(courses)
 })
 
 // /api/courses/1
-app.get('/api/posts/:year/:month', (req, res) => {
-  res.send(req.query)
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id))
+  if (!course) res.status(404).send('The course was not found')
+  res.send(course)
 })
 
 // PORT
