d53bde900cc1cbed39e240770417aa8799455877
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-06-27 09:23:26 +0100

    using Joi for validation and error messages

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const Joi = require('joi')
 const express = require('express')
 const app = express()
+
+app.use(express.json())
 
 const courses = [
   { id: 1, name: 'course1' },
@@ -13,6 +16,26 @@ app.get('/', (req, res) => {
 
 app.get('/api/courses', (req, res) => {
   res.send(courses)
+})
+
+app.post('/api/courses', (req, res) => {
+  const schema = {
+    name: Joi.string().min(3).required()
+  }
+
+  const result = Joi.validate(req.body, schema)
+
+  if (result.error) {
+    // 400 bad request
+    res.status(400).send(result.error.details[0].message)
+    return
+  }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  }
+  courses.push(course)
+  res.send(course)
 })
 
 // /api/courses/1
