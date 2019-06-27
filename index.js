514fc12f68964e47d2bfa3fa66acfb36c8b3e23b
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-06-27 11:22:26 +0100

    delete method added

--- index.js
+++ index.js
@@ -75,3 +75,19 @@ function validateCourse(course) {
   }
   return Joi.validate(course, schema)
 }
+
+app.delete('api/courses/:id', (req, res) => {
+  //Look up the course
+  //Not existing, return 404
+  const course = courses.find(c => c.id === parseInt(req.params.id))
+  if (!course) res.status(404).send('The course was not found')
+
+  //Delete
+  const index = courses.indexOf(course)
+  courses.splice(index, 1)
+
+
+  //Return the same course
+  res.send(course)
+
+})
